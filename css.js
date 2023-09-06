2794df8b549e3bbf3943a5ef283c1ad000db52ef
taifletcher <35815558+taifletcher@users.noreply.github.com>
2023-09-05 22:51:24 -0400

    Update css.js

--- css.js
+++ css.js
@@ -21,7 +21,31 @@ function preload() {
 
 //-- usage --//
 preload(
-    "http://domain.tld/gallery/image-001.jpg",
-    "http://domain.tld/gallery/image-002.jpg",
-    "http://domain.tld/gallery/image-003.jpg"
+"images/Row3_000.jpg"
+"images/Row3_001.jpg",
+"images/Row3_002.jpg",
+"images/Row3_003.jpg",
+"images/Row3_004.jpg",
+"images/Row3_005.jpg",
+"images/Row3_006.jpg",
+"images/Row3_007.jpg",
+"images/Row3_008.jpg",
+"images/Row3_009.jpg",
+"images/Row3_010.jpg",
+"images/Row3_011.jpg",
+"images/Row3_012.jpg",
+"images/Row3_013.jpg",
+"images/Row3_014.jpg",
+"images/Row3_015.jpg",
+"images/Row3_016.jpg",
+"images/Row3_017.jpg",
+"images/Row3_018.jpg",
+"images/Row3_019.jpg",
+"images/Row3_020.jpg",
+"images/Row3_021.jpg",
+"images/Row3_022.jpg",
+"images/Row3_023.jpg",
+"images/Row3_024.jpg",
+
+ 
 )
